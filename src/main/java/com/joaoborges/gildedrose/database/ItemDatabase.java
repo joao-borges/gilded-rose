2cdf87fc9fa021019e209900f87d9d60585d8288
joaoborges <joaoborges@touchtec.com.br>
2020-01-09 18:10:07 -0800

    Removing incorrect copyright information
    Change-Id: I59075526473b83309ae213a0e690f53c3611b100

--- src/main/java/com/joaoborges/gildedrose/database/ItemDatabase.java
+++ src/main/java/com/joaoborges/gildedrose/database/ItemDatabase.java
@@ -1,13 +1,4 @@
-/*
- * Copyright (c) 1999-2018 Touch Tecnologia e Informatica Ltda.
- *
- * R. Gomes de Carvalho, 1666, 3o. Andar, Vila Olimpia, Sao Paulo, SP, Brasil.
- *
- * Todos os direitos reservados.
- * Este software e confidencial e de propriedade da Touch Tecnologia e Informatica Ltda. (Informacao Confidencial)
- * As informacoes contidas neste arquivo nao podem ser publicadas, e seu uso esta limitado de acordo
- * com os termos do contrato de licenca.
- */
+
 package com.joaoborges.gildedrose.database;
 
 import static java.util.stream.Collectors.toList;
